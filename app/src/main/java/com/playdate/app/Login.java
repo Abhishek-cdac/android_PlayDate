4d3d23139492a0c66c57be2e0c0a0f9fd4b749b9
Tushti <tushti.purna@nectarinfotel.com>
2021-04-16 14:19:26 +0530

    Removed hardcord data

--- app/src/main/java/com/playdate/app/Login.java
+++ app/src/main/java/com/playdate/app/Login.java
@@ -2,7 +2,22 @@ package com.playdate.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +25,51 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText password = findViewById(R.id.login_Password);
+        Button login_btn = findViewById(R.id.login_button);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("OnClick", "onClick: ");
+            }
+        });
+
+        printHashKey();
+
+//        password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                if(hasFocus){
+//                    Log.d("Password", "onFocusChange: ");
+//                    password.setHint(" ");
+//                }else{
+//                    password.setHint("Password");
+//                }
+//            }
+//        });
     }
+
+    public void printHashKey() {
+        try {
+
+            PackageInfo info
+                    = getPackageManager().getPackageInfo(
+                    "com.android.facebookloginsample",
+                    PackageManager.GET_SIGNATURES);
+
+            for (Signature signature : info.signatures) {
+
+                MessageDigest md
+                        = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:",
+                        Base64.encodeToString(
+                                md.digest(),
+                                Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        } catch (NoSuchAlgorithmException e) {
+        }
+    }
+
 }
