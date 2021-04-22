25a4d1d7b65f3cf3c31aebeca231170d92f92d1b
Ajit <ajit.jadhav36@gmail.com>
2021-04-22 17:31:05 +0530

    landing

--- app/src/main/java/com/playdate/app/ui/register/username/UserNameActivity.java
+++ app/src/main/java/com/playdate/app/ui/register/username/UserNameActivity.java
@@ -2,29 +2,87 @@ package com.playdate.app.ui.register.username;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 
 import com.playdate.app.R;
+import com.playdate.app.databinding.ActivityUserDetailsBinding;
+import com.playdate.app.databinding.ActivityUsernameBinding;
 import com.playdate.app.ui.register.bio.BioActivity;
 import com.playdate.app.ui.register.interestin.InterestActivity;
 import com.playdate.app.ui.register.relationship.RelationActivity;
+import com.playdate.app.util.common.CommonClass;
 
 public class UserNameActivity extends AppCompatActivity {
+    UserNameViewModel userNameViewModel;
+    ActivityUsernameBinding binding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_username);
-        ImageView iv_next = findViewById(R.id.iv_next);
-        iv_next.setOnClickListener(new View.OnClickListener() {
+        userNameViewModel = new UserNameViewModel();
+        binding = DataBindingUtil.setContentView(UserNameActivity.this, R.layout.activity_username);
+        binding.setLifecycleOwner(this);
+        binding.setUserNameViewModel(userNameViewModel);
+
+        userNameViewModel.OnNextClick().observe(this, new Observer<Boolean>() {
             @Override
-            public void onClick(View view) {
+            public void onChanged(Boolean click) {
                 startActivity(new Intent(UserNameActivity.this, BioActivity
                         .class));
+
             }
         });
+        userNameViewModel.OnUserNameInput().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String charSeq) {
+                if (charSeq.length() == 5) {
+                    startTimer();
+                } else {
+//                    binding.spinKit.setVisibility(View.GONE);
+                }
+
+            }
+        });
+
+//        iv_next
+
+        userNameViewModel.onBackClick().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean click) {
+                finish();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    Handler handler;
+
+    private void startTimer() {
+        binding.spinKit.setVisibility(View.VISIBLE);
+        new CommonClass().hideKeyboard(binding.spinKit, this);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.spinKit.setVisibility(View.GONE);
+                binding.ivNext.setVisibility(View.VISIBLE);
+                binding.edtFullname.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.check, 0);
+
+            }
+        }, 3000);
     }
 }
