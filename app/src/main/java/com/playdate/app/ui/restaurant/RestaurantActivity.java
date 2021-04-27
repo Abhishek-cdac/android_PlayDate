4fd6f14eda9b2a3c28a8c7b2847ac6d65f79ef6b
Tushti <tushti.purna@nectarinfotel.com>
2021-04-27 13:53:08 +0530

    Filters in restaurant and interest

--- app/src/main/java/com/playdate/app/ui/restaurant/RestaurantActivity.java
+++ app/src/main/java/com/playdate/app/ui/restaurant/RestaurantActivity.java
@@ -2,6 +2,8 @@ package com.playdate.app.ui.restaurant;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +14,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.playdate.app.R;
 import com.playdate.app.databinding.ActivityRestaurantBinding;
+import com.playdate.app.model.Interest;
 import com.playdate.app.ui.dashboard.DashboardActivity;
+import com.playdate.app.ui.restaurant.adapter.Restaurant;
 import com.playdate.app.ui.restaurant.adapter.RestaurantAdapter;
+
+import java.util.ArrayList;
 
 public class RestaurantActivity extends AppCompatActivity {
     RestaurantViewModel viewModel;
     ActivityRestaurantBinding binding;
+
+    ArrayList<Restaurant> rest_list = new ArrayList<>();
+    RestaurantAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,7 +36,7 @@ public class RestaurantActivity extends AppCompatActivity {
         binding.setLifecycleOwner(this);
         binding.setRestaurantViewModel(viewModel);
 
-        RestaurantAdapter adapter = new RestaurantAdapter();
+        init();
 
         // bind RecyclerView
         RecyclerView recyclerView = binding.recyclerviewInterest;
@@ -35,7 +44,25 @@ public class RestaurantActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
 //        recyclerView.addItemDecoration(new SpacesItemDecoration(15));
 
+        adapter = new RestaurantAdapter(rest_list);
         recyclerView.setAdapter(adapter);
+
+        binding.edtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
 
 
         viewModel.OnNextClick().observe(RestaurantActivity.this, new Observer<Boolean>() {
@@ -51,6 +78,31 @@ public class RestaurantActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    private void filter(String str) {
+        if(str.length()>0){
+            ArrayList<Restaurant> filteredNames = new ArrayList<>();
+            for (Restaurant s : rest_list) {
+                if (s.getName().toLowerCase().contains(str.toLowerCase())) {
+                    filteredNames.add(s);
+                }
+            }
+            adapter.updateList(filteredNames);
+            adapter.notifyDataSetChanged();
+        }else{
+            adapter.updateList(rest_list);
+            adapter.notifyDataSetChanged();
+        }
+
+    }
+
+    private void init() {
+        rest_list.add(new Restaurant(R.drawable.rest1, "Benihana grill", false));
+        rest_list.add(new Restaurant(R.drawable.rest2, "olive garden", false));
+        rest_list.add(new Restaurant(R.drawable.rest3, "tete restaurant", false));
+        rest_list.add(new Restaurant(R.drawable.rest4, "friendly's", false));
+        rest_list.add(new Restaurant(R.drawable.rest5, "the taste of mexico montezuma's", false));
+        rest_list.add(new Restaurant(R.drawable.rest6, "arbys", false));
     }
 }
--- app/src/main/java/com/playdate/app/ui/restaurant/adapter/Restaurant.java
+++ app/src/main/java/com/playdate/app/ui/restaurant/adapter/Restaurant.java
@@ -2,13 +2,23 @@ package com.playdate.app.ui.restaurant.adapter;
 
 public class Restaurant {
     int image;
+    String name;
     boolean selected;
 
-    public Restaurant(int image, boolean selected) {
+    public Restaurant(int image, String name, boolean selected) {
         this.image = image;
+        this.name = name;
         this.selected = selected;
 
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getImage() {
