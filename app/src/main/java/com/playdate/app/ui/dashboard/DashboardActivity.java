6491edd33e3c14c09eb7fa51014a6b01d169a4a3
Ajit <ajit.jadhav36@gmail.com>
2021-04-29 17:18:12 +0530

    Code merge for the fragments

--- app/src/main/java/com/playdate/app/ui/dashboard/DashboardActivity.java
+++ app/src/main/java/com/playdate/app/ui/dashboard/DashboardActivity.java
@@ -1,34 +1,84 @@
 package com.playdate.app.ui.dashboard;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.playdate.app.R;
 import com.playdate.app.ui.card_swipe.FragCardSwipeActivity;
+import com.playdate.app.ui.dashboard.adapter.FriendAdapter;
 import com.playdate.app.ui.dashboard.fragments.FragLanding;
 import com.playdate.app.ui.dashboard.more_suggestion.FragMoreSuggestion;
+import com.playdate.app.ui.interfaces.OnInnerFragmentClicks;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements OnInnerFragmentClicks {
+    FragmentManager fm;
+    FragmentTransaction ft;
+    TextView txt_match;
+    TextView txt_social;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        txt_social = findViewById(R.id.txt_social);
+        txt_match = findViewById(R.id.txt_match);
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
+        RecyclerView rv_friends = findViewById(R.id.rv_friends);
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        FriendAdapter adapterfriend = new FriendAdapter();
+        rv_friends.setAdapter(adapterfriend);
+        rv_friends.setLayoutManager(manager);
 
-//        Fragment fragOne = new FragLanding();
+        Fragment fragOne = new FragLanding();
 //        Fragment fragOne = new FragMoreSuggestion();
-        Fragment fragOne = new FragCardSwipeActivity();
-        Bundle arguments = new Bundle();
-        arguments.putBoolean("shouldYouCreateAChildFragment", true);
-        fragOne.setArguments(arguments);
+//        Fragment fragOne = new FragCardSwipeActivity();
         ft.add(R.id.flFragment, fragOne);
         ft.commit();
+
+        txt_match.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt_social.setBackground(null);
+                txt_match.setTextColor(getResources().getColor(R.color.white));
+                txt_social.setTextColor(getResources().getColor(android.R.color.darker_gray));
+                txt_match.setBackground(getResources().getDrawable(R.drawable.menu_button));
+                ReplaceFrag(new FragCardSwipeActivity());
+            }
+        });
+        txt_social.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt_match.setBackground(null);
+                txt_social.setTextColor(getResources().getColor(R.color.white));
+                txt_match.setTextColor(getResources().getColor(android.R.color.darker_gray));
+                txt_social.setBackground(getResources().getDrawable(R.drawable.menu_button));
+//                ReplaceFrag(new FragCardSwipeActivity());
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public void ReplaceFrag(Fragment fragment) {
+        ft = fm.beginTransaction();
+        ft.replace(R.id.flFragment, fragment, fragment.getClass().getSimpleName());
+        ft.addToBackStack("tags");
+        ft.commitAllowingStateLoss();
     }
 }
+
+
+
--- app/src/main/java/com/playdate/app/ui/dashboard/fragments/FragLanding.java
+++ app/src/main/java/com/playdate/app/ui/dashboard/fragments/FragLanding.java
@@ -1,6 +1,5 @@
 package com.playdate.app.ui.dashboard.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,16 +9,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.playdate.app.R;
-import com.playdate.app.ui.dashboard.DashboardActivity;
-import com.playdate.app.ui.dashboard.adapter.FriendAdapter;
 import com.playdate.app.ui.dashboard.adapter.SuggestionAdapter;
 import com.playdate.app.ui.dashboard.more_suggestion.FragMoreSuggestion;
-import com.playdate.app.ui.record_video.RecordVideoActivity;
+import com.playdate.app.ui.interfaces.OnInnerFragmentClicks;
 
 public class FragLanding extends Fragment {
 
@@ -28,11 +23,7 @@ public class FragLanding extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_landing, container, false);
         ViewPager vp_suggestion = view.findViewById(R.id.vp_suggestion);
-        RecyclerView rv_friends = view.findViewById(R.id.rv_friends);
-        RecyclerView.LayoutManager manager = new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false);
-        FriendAdapter adapterfriend = new FriendAdapter();
-        rv_friends.setAdapter(adapterfriend);
-        rv_friends.setLayoutManager(manager);
+
 
 //        SuggestionAdapter adapter = new SuggestionAdapter(getActivity(), getActivity().getSupportFragmentManager());
         SuggestionAdapter adapter = new SuggestionAdapter(getActivity());
@@ -40,13 +31,15 @@ public class FragLanding extends Fragment {
         vp_suggestion.setCurrentItem(8);
         vp_suggestion.setPadding(130, 0, 130, 0);
 
-//        TextView footer_see_more = view.findViewById(R.id.txt_footer_see_more);
-//        footer_see_more.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
+        TextView txt_footer_see_more = view.findViewById(R.id.txt_footer_see_more);
+        txt_footer_see_more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                OnInnerFragmentClicks frag = (OnInnerFragmentClicks) getActivity();
+                frag.ReplaceFrag(new FragMoreSuggestion());
+            }
+        });
 
 
         return view;
