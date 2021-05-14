50c52bb029976a87215c6be8df7fc0e0b57e283d
Tushti <tushti.purna@nectarinfotel.com>
2021-05-14 14:08:23 +0530

    Local commit

---
+++ app/src/main/java/com/playdate/app/ui/date/SelectPartner.java
@@ -0,0 +1,16 @@
+package com.playdate.app.ui.date;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.playdate.app.R;
+
+public class SelectPartner extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_partner);
+    }
+}
