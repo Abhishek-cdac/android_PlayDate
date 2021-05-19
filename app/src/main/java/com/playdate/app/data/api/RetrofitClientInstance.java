1497e9dffbe0291d957a9817bb33131a681498fe
Ajit <ajit.jadhav@nectarinfotel.com>
2021-05-19 12:33:45 +0530

    Added logger in retrofit

--- app/src/main/java/com/playdate/app/data/api/RetrofitClientInstance.java
+++ app/src/main/java/com/playdate/app/data/api/RetrofitClientInstance.java
@@ -14,13 +14,12 @@ public class RetrofitClientInstance {
     public static final String BASE_URL_IMAGE = "http://139.59.0.106:3000";
     public static final String DEVICE_TYPE = "Android";
 
-//    HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-//    logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
 
     private static OkHttpClient getRequestHeader() {
         return new OkHttpClient.Builder()
                 .readTimeout(60, TimeUnit.SECONDS)
                 .connectTimeout(60, TimeUnit.SECONDS)
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
                 .writeTimeout(60, TimeUnit.SECONDS)
                 .build();
     }
