1f3c53f444da775364bd455ebd726d6013f16e49
Mayuri Dayla <mayuri.dayla@nectarinfotel.com>
2021-07-22 16:37:39 +0530

    null check on coupon valid date, business image full view, change text on profile

--- app/src/main/java/com/playdate/app/business/couponsGenerate/adapter/ActiveCouponsAdapter.java
+++ app/src/main/java/com/playdate/app/business/couponsGenerate/adapter/ActiveCouponsAdapter.java
@@ -28,7 +28,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class ActiveCouponsAdapter extends RecyclerView.Adapter<ActiveCouponsAdapter.ViewHolder> {
-  //  ArrayList<ActiveCoupons> list = new ArrayList<>();
+    //  ArrayList<ActiveCoupons> list = new ArrayList<>();
     ArrayList<GetBusinessCouponData> list = new ArrayList<>();
     FragActiveCoupons fragActiveCoupons;
     Context mcontext;
@@ -37,55 +37,31 @@ public class ActiveCouponsAdapter extends RecyclerView.Adapter<ActiveCouponsAdap
 
     public ActiveCouponsAdapter(ArrayList<GetBusinessCouponData> getBusinessCouponLst, FragActiveCoupons fragActiveCoupons) {
         this.list = getBusinessCouponLst;
-        this.fragActiveCoupons =fragActiveCoupons;
+        this.fragActiveCoupons = fragActiveCoupons;
         picasso = Picasso.get();
-//        list.add(new ActiveCoupons("Retro Dinner", "Enjoy 15% off on our special meals and drinks", "24/09", R.drawable.rest_1));
-//        list.add(new ActiveCoupons("Retro Dinner", "Enjoy 15% off on our special meals and drinks", "18/09", R.drawable.rest_1));
-//        list.add(new ActiveCoupons("Retro Dinner", "Enjoy 15% off on our special meals and drinks", "16/09", R.drawable.rest_1));
-//        list.add(new ActiveCoupons("Retro Dinner", "Enjoy 15% off on our special meals and drinks", "14/09", R.drawable.rest_1));
-//        list.add(new ActiveCoupons("Retro Dinner", "Enjoy 15% off on our special meals and drinks", "10/09", R.drawable.rest_1));
-//        list.add(new ActiveCoupons("Retro Dinner", "Enjoy 15% off on our special meals and drinks", "08/09", R.drawable.rest_1));
-//        list.add(new ActiveCoupons("Retro Dinner", "Enjoy 15% off on our special meals and drinks", "06/09", R.drawable.rest_1));
-//        list.add(new ActiveCoupons("Retro Dinner", "Enjoy 15% off on our special meals and drinks", "04/09", R.drawable.rest_1));
-//        list.add(new ActiveCoupons("Retro Dinner", "Enjoy 15% off on our special meals and drinks", "02/09", R.drawable.rest_1));
     }
 
     @NonNull
     @Override
     public ActiveCouponsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_active_coupon, parent, false);
-       mcontext = parent.getContext();
+        mcontext = parent.getContext();
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ActiveCouponsAdapter.ViewHolder holder, int position) {
-
-        String[] s = list.get(position).getCouponValidTillDate().split("T");
-
-        Log.e("getCouponValidTillDate",""+s);
-        DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
-         //readDate = null;
-        try {
-            Date  readDate = df.parse(s[0]);
-            Calendar cal = Calendar.getInstance();
-            cal.setTimeInMillis(readDate.getTime());
-            Log.d("TAG", "Year: "+cal.get(Calendar.YEAR));
-            Log.d("TAG", "Month: "+cal.get(Calendar.MONTH));
-            Log.d("TAG", "Day: "+cal.get(Calendar.DAY_OF_MONTH));
-
-        } catch (ParseException e) {
-            e.printStackTrace();
+        if (list.get(position).getCouponValidTillDate() != null) {
+            String[] s = list.get(position).getCouponValidTillDate().split("T");
+            Log.e("getCouponValidTillDate", "" + s[0]);
+            holder.tv_date.setText(s[0]);
         }
 
-
-
-        holder.tv_date.setText(s[0]);
-        picasso.load(list.get(position).getCouponImage()).placeholder(R.drawable.ic_baseline_person_24)
+        picasso.load(list.get(position).getCouponImage()).placeholder(R.drawable.rest_1)
                 .fit()
                 .centerCrop()
                 .into(holder.iv_image);
-      //  holder.tv_date.setText(list.get(position).getCouponValidTillDate());
+        //  holder.tv_date.setText(list.get(position).getCouponValidTillDate());
         holder.rest_name.setText(list.get(position).getUser().get(0).getFullName());
         holder.discount_desc.setText(list.get(position).getCouponTitle());
         holder.status.setText(list.get(position).getStatus());
@@ -100,7 +76,8 @@ public class ActiveCouponsAdapter extends RecyclerView.Adapter<ActiveCouponsAdap
         ImageView iv_image;
         TextView rest_name;
         TextView discount_desc;
-        TextView tv_date; TextView status;
+        TextView tv_date;
+        TextView status;
         RelativeLayout rl_coupons;
 
         public ViewHolder(@NonNull View itemView) {
